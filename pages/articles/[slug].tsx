bb38eefa2fb981621e1fa29be30b856de5839786
ayumubanban <ayumuban@outlook.com>
2022-08-13 20:12:34 +0900

    feat: :sparkles: Fetch blocks by page id

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -5,18 +5,23 @@ import { GetStaticProps, NextPage } from "next";
 import Layout from "../../components/Layout";
 import ArticleMeta from "../../components/ArticleMeta";
 import { ArticleProps, Params } from "../../types/types";
-import { sampleCards } from "../../utils/sample";
+import { fetchBlocksByPageId, fetchPages } from "../../utils/notion";
 
 // export const getServerSideProps: GetServerSideProps = async (ctx) => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params as Params; // [slug].tsx
 
-  const page = sampleCards.find((data) => data.slug === slug);
+  const { results } = await fetchPages({ slug }); // 例えば slug が同じ記事が複数あった場合、2件以上の記事のデータが返ってくることもありうる。まぁ何にせよ、この返り値の型は配列となる
+  const page = results[0];
+  const pageId = page.id;
+  const { results: blocks } = await fetchBlocksByPageId(pageId);
 
   return {
     props: {
       page,
+      blocks,
     },
+    revalidate: 10, // ISR
   };
 };
 
