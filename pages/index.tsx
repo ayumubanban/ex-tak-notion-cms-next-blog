2e8eec0ef4672286499b71104a4f259d0ec884c6
ayumubanban <ayumuban@outlook.com>
2022-08-13 09:59:32 +0900

    feat: :sparkles: Card component
    24. Cardコンポーネント https://www.udemy.com/course/notion-next-blog/learn/lecture/32989368#overview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Card from "../components/Card";
 
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
@@ -12,7 +13,7 @@ const Home: NextPage = () => {
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
           {sampleCards.map((page, index) => (
-            <p key={index}>{page.name}</p>
+            <Card key={index} page={page} />
           ))}
         </div>
       </div>
